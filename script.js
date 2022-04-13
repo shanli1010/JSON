46c24d37feafb3a140e8db34c6cf5438092dbc58
shanli1010 <90589845+shanli1010@users.noreply.github.com>
2022-04-12 23:48:16 -0400

    update

--- script.js
+++ script.js
@@ -1,1 +1,72 @@
-s
+let favs = document.getElementById('charts');
+
+function printToPage(incoming) {
+  outputParagraph.innerHTML = incoming;
+}
+
+let jsonDatabase = [
+  {
+    "Genre" : "Hip-Hop/Rap"
+    "Song" : "Leray",
+    "AlbumCover" : "Leray.jpg",
+    "Artist" : "Trippie Redd",
+  },
+  {
+    "Genre" : "R&B/Soul"
+    "Song" : "SLOW DANCING IN THE DARK",
+    "AlbumCover" : "joji.jpg",
+    "Artist" : "Joji",
+  },
+  {
+    "Genre" : "Alternative Rock"
+    "Song" : "Check Yes, Juliet",
+    "AlbumCover" : "WeTheKings.jpeg",
+    "Artist" : "We The Kings",
+  },
+  {
+    "Genre" : "Classical"
+    "Song" : "Clair De Lune",
+    "AlbumCover" : "clairDeLune.jpeg",
+    "Artist" : "Claude Debussy",
+]
+
+for (var i = 0; i < jsonDatabase.length; i++) {
+  createElement(jsonDatabase[i]);
+}
+
+  function createElements(incomingJSON) {
+
+    //items
+    let  = document.createElement("div");
+    contentElement.classList.add('contentItem');
+
+    //Genre
+    let genre = document.createElement("H2");
+    genre.classList.add('contentTitle');
+    genre.innerText = incomingJSON['Genre'];
+    contentElement.appendChild(genre);
+
+    //AlbumCover
+    let img = document.createElement("IMG");
+    img.classList.add('minionsImage');
+    img.src = incomingJSON['imageURL'];
+    contentElement.appendChild(img);
+
+    let newList = document.createElement("div");
+    newList.appendChild(newContentSingersList);
+
+    //Title
+    let title = document.createElement("H3");
+    title.classList.add('contentTitle');
+    title.innerText = "Title: " + incomingJSON['Song'];
+    contentElement.appendChild(title);
+
+    //Artist
+    let artist = document.createElement("H3");
+    artist.classList.add('contentTitle');
+    artist.innerText = "Artist: " + incomingJSON['Artist'];
+    contentElement.appendChild(artist);
+
+
+   favs.appendChild(contentElement);
+  }
